24af52663faac571cf8d48b3293b51e43406979b
Tyler <tbrust@ilstu.edu>
2019-10-15 15:41:33 -0500

    Ty;er

---
+++ ComboBoxTest.java
@@ -0,0 +1,117 @@
+/*import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class ComboBoxTest extends JPanel implements ActionListener
+{
+	
+	public ComboBoxTest() {
+    super(new BorderLayout());
+	JFrame frame = new JFrame("Alarms");
+	JPanel panel = new JPanel();
+	frame.add(panel);
+	
+	
+	
+    String[] hourStrings = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+    String[] minuteStrings = {"00", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
+    		"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33",
+    		"34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51",
+    		"52", "53", "54", "55", "56", "57", "58", "59",};
+    String[] dayNightStrings = {"AM", "PM"};
+    String[] monthStrings = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+    String[] dayStrings = {"00", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
+    		"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+    String[] yearStrings = {"2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"};
+
+    
+    
+    
+    //Create the combo box, select the item at index 4.
+    //Indices start at 0, so 4 specifies the pig.
+    JComboBox hourList = new JComboBox(hourStrings);
+    hourList.setSelectedIndex(0);
+    hourList.addActionListener(this);
+    
+    JComboBox minuteList = new JComboBox(minuteStrings);
+    minuteList.setSelectedIndex(0);
+    minuteList.addActionListener(this);
+    
+    JComboBox dayNightList = new JComboBox(dayNightStrings);
+    dayNightList.setSelectedIndex(0);
+    dayNightList.addActionListener(this);
+    
+	JTextField optionalMessage = new JTextField();
+	optionalMessage.setEditable(true);
+	optionalMessage.setColumns(15);
+
+    JComboBox monthList = new JComboBox(monthStrings);
+    monthList.setSelectedIndex(0);
+    monthList.addActionListener(this);
+    
+    JComboBox dayList = new JComboBox(dayStrings);
+    dayList.setSelectedIndex(0);
+    dayList.addActionListener(this);
+    
+    JComboBox yearList = new JComboBox(yearStrings);
+    yearList.setSelectedIndex(0);
+    yearList.addActionListener(this);
+    
+    
+    
+    
+    
+    
+	
+	JLabel label = new JLabel("Hour");
+	panel.add(label);
+    panel.add(hourList);
+    
+	JLabel label2 = new JLabel("Minute");
+	panel.add(label2);
+    panel.add(minuteList);
+    
+	JLabel label3 = new JLabel("AM/PM");
+	panel.add(label3);
+    panel.add(dayNightList);
+    
+    JLabel label5 = new JLabel("Month");
+    panel.add(label5);
+    panel.add(monthList);
+    
+    JLabel label6 = new JLabel("Day");
+    panel.add(label6);
+    panel.add(dayList);
+        
+    JLabel label7 = new JLabel("Year");
+    panel.add(label7);
+    panel.add(yearList);
+    
+    JLabel label4 = new JLabel("Optional Message");
+    panel.add(label4);
+	panel.add(optionalMessage);   
+    
+	JButton button = new JButton("Submit");
+    panel.add(button);  
+    
+    
+    
+	frame.setSize(325,175);
+	frame.setVisible(true);
+	}
+	
+	
+	
+	
+    public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        String hourNum = (String)cb.getSelectedItem();
+    }
+    
+    
+	public static void main(String args[]) {
+		ComboBoxTest cbt = new ComboBoxTest();
+	}
+}
+*/
