204087662af1d597db8396044976eb9abfd0e99c
Matthew0898 <Matthew0898@Matthew0898.com>
2019-10-15 23:34:30 -0500

    Created Daemon to check for running events

--- src/Run.java
+++ src/Run.java
@@ -1,6 +1,12 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Run {
 	public static void main(String args[]) {
 		AlarmFrame af = new AlarmFrame();
+		Timer timer=new Timer(true);
+		TimerTask task=null;
+		timer.schedule(task,10000);
+		
 	}
 }
---
+++ src/alarmFirer.java
@@ -0,0 +1,11 @@
+import java.util.TimerTask;
+
+public class alarmFirer extends TimerTask {
+
+	@Override
+	public void run() {
+		//if Alarm PreviousorCurrent
+
+	}
+
+}
